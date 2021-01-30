ba65af16fa21b1c51bba2a05b54a2fdf5b306101
Tân Phạm <tanpham1104@gmail.com>
2021-01-30 10:13:10 +0700

    feat: add detail page logic

--- models/product.js
+++ models/product.js
@@ -23,6 +23,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -33,5 +34,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductFromFile((products) => {
+            const product = products.find((p) => p.id === id);
+            cb(product);
+        });
     }
 };
