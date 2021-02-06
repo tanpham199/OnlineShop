f1ef5cdc5e77e864d52e47af3826fb5adc3e7363
Tân Phạm <tanpham1104@gmail.com>
2021-02-06 23:55:38 +0700

    refactor: switch to mongodb - update, delete, create features

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,44 +19,37 @@ exports.postAddProduct = async (req, res, next) => {
     }
 };
 
-// exports.getEditProduct = async (req, res, next) => {
-//     const editMode = req.query.edit;
-//     if (!editMode) {
-//         return res.redirect('/');
-//     }
-//     try {
-//         // const product = await Product.findByPk(req.params.productId); // normal quey without binding user
-//         const products = await req.user.getProducts({ where: { id: req.params.productId } }); // this returns an array
-//         const product = products[0]; // we know for sure that result array only has one item
-//         if (!product) {
-//             return res.render('404');
-//         }
-//         res.render('admin/edit-product', {
-//             pageTitle: 'Add Product',
-//             path: '/admin/edit-product',
-//             editing: editMode,
-//             product: product,
-//         });
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
+exports.getEditProduct = async (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    try {
+        const product = await Product.findById(req.params.productId);
+        if (!product) {
+            return res.render('404');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Add Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-// exports.postEditProduct = async (req, res, next) => {
-//     const { productId, title, price, imageUrl, description } = req.body;
-//     try {
-//         const product = await Product.findByPk(productId);
-//         product.title = title;
-//         product.price = price;
-//         product.imageUrl = imageUrl;
-//         product.description = description;
-
-//         await product.save(); // create new if not existed
-//         res.redirect('/admin/products');
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
+exports.postEditProduct = async (req, res, next) => {
+    const { productId, title, price, imageUrl, description } = req.body;
+    try {
+        const product = new Product(title, price, imageUrl, description, productId);
+        await product.save();
+        res.redirect('/admin/products');
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 exports.getProducts = async (req, res, next) => {
     try {
@@ -71,12 +64,11 @@ exports.getProducts = async (req, res, next) => {
     }
 };
 
-// exports.postDeleteProduct = async (req, res, next) => {
-//     try {
-//         const product = await Product.findByPk(req.body.productId);
-//         await product.destroy();
-//         res.redirect('/admin/products');
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
+exports.postDeleteProduct = async (req, res, next) => {
+    try {
+        await Product.deleteById(req.body.productId);
+        res.redirect('/admin/products');
+    } catch (err) {
+        console.log(err);
+    }
+};
